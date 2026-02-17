a97cd3d72ea5785b9a11345542f4b96e0545a9f5
Giovanna Siqueira <giihsiq.contato@gmail.com>
2026-02-17 15:30:10 -0300

    feat: Exercício de laços de repetição - while

---
+++ logica-de-programacao/src/devdojo/lacosderepeticao/whiles/Login.java
@@ -0,0 +1,48 @@
+package devdojo.lacosderepeticao.whiles;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Login {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * [PT-BR] O usuário deve acertar o login de usuário e senha armazenado em constantes - "Acesso Liberado" caso acerte, e "Acesso Negado" caso erre, até acertar o login correto.
+		 * [ENG] The user wants
+		 * [ESP] El usuario desea
+		 */
+
+		Locale.setDefault(Locale.US);
+		Scanner scanner = new Scanner(System.in);
+		
+		/* Constante = não pode ser alterado */
+		final String usuario = "admin";
+		final String senha = "admin";
+		
+		System.out.print("Usuário: ");
+		String usuarioInserido = scanner.nextLine();
+		
+		System.out.print("Senha: ");
+		String senhaInserida = scanner.nextLine();
+		
+		int i = 0;
+		
+		while (!usuarioInserido.equals(usuario) || !senhaInserida.equals(senha)) {
+			i = i + 1;
+			System.out.println("Acesso Negado.");
+			
+			System.out.print("Usuário: ");
+			usuarioInserido = scanner.nextLine();
+			
+			System.out.print("Senha: ");
+			senhaInserida = scanner.nextLine();
+		}
+		System.out.println("Acesso Liberado.");
+		
+		
+		scanner.close();
+		
+	}
+
+}
