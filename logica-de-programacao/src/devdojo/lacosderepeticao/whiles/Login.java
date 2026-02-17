6ea4b0856c413dfd46843bc74986b0302f611ae4
Giovanna Siqueira <giihsiq.contato@gmail.com>
2026-02-17 15:32:53 -0300

    feat: Updated last commit

--- logica-de-programacao/src/devdojo/lacosderepeticao/whiles/Login.java
+++ logica-de-programacao/src/devdojo/lacosderepeticao/whiles/Login.java
@@ -9,8 +9,8 @@ public class Login {
 		
 		/*
 		 * [PT-BR] O usuário deve acertar o login de usuário e senha armazenado em constantes - "Acesso Liberado" caso acerte, e "Acesso Negado" caso erre, até acertar o login correto.
-		 * [ENG] The user wants
-		 * [ESP] El usuario desea
+		 * [ENG] The user must enter the username and password stored in constants - “Access Granted” if correct, and “Access Denied” if incorrect, until the correct login is entered.
+		 * [ESP] El usuario debe introducir correctamente el nombre de usuario y la contraseña almacenados en constantes: "Acceso permitido" si acierta y "Acceso denegado" si se equivoca, hasta que introduzca los datos correctos.
 		 */
 
 		Locale.setDefault(Locale.US);
